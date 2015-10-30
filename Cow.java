efeea5729cdb01c202b621be6bca00989b963656
kellyhuang21 <kellyh21stuff@gmail.com>
2015-10-29 22:31:00 -0700

    protected

--- Cow.java
+++ Cow.java
@@ -1,14 +1,16 @@
-class Cow implements Animal {     
-     private String myType;     
-     private String mySound;      
-     public Cow(String type, String sound)     {         
+class Cow implements Animal{     
+     protected String myType;     
+     protected String mySound;      
+     public Cow(String type, String sound){         
          myType = type;         
          mySound = sound;     
      }     
-     public Cow()     {         
+     public Cow(){         
          myType = "Cows";         
          mySound = "moo";     
      }      
-     public String getSound(){return mySound;}     
-     public String getType(){return myType;} 
+     public String getSound(){return mySound;
+     }     
+     public String getType(){return myType;
+     } 
 }
